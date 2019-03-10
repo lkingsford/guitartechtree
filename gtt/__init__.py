ce83f7a962bae746fa0d74629a24a5c0ffeba9fa
Lachlan Kingsford <lachlan@nerdygentleman.com>
2019-03-10 23:24:55 +1100

    Added almost empty layout and favicons

--- gtt/__init__.py
+++ gtt/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True,
+    static_url_path='/static')
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 app.secret_key = app.config['SESSION_SECRET_KEY']
